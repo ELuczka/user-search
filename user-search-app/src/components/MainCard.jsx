77d90b2cd9a81e04f5d581dfd1ff7a1162156697
Ewelina Luczka <e.luczka@wp.pl>
2020-08-26 17:33:00 +0200

    Extract Serch to new component

--- user-search-app/src/components/MainCard.jsx
+++ user-search-app/src/components/MainCard.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 
-import SearchIcon from "@material-ui/icons/Search";
-import { Button, InputBase, Paper } from "@material-ui/core";
+import UserSearch from './UserSearch';
 
 const MainCard = () => {
   return (
     <div>
-      <Paper>
-        <SearchIcon />
-        <InputBase placeholder="Search for users" />
-      </Paper>
-      <Button variant="contained" size="medium" color="primary">
-        Search
-      </Button>
+      <UserSearch/>
     </div>
   );
 };
